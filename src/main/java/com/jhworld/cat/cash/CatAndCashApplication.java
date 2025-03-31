999b6cf9b4214e966cf83c25ea50cac93f5ad684
SHKim55 <5061ksh@naver.com>
2025-03-31 22:26:12 +0900

    feat : first commit

---
+++ src/main/java/com/jhworld/cat/cash/CatAndCashApplication.java
@@ -0,0 +1,13 @@
+package com.jhworld.cat.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAndCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatAndCashApplication.class, args);
+	}
+
+}
