f083b17f91630a4de35012f4d97c20c43d50ad3b
SHKim55 <5061ksh@naver.com>
2025-03-31 22:26:12 +0900

    feat : first commit

---
+++ src/test/java/com/jhworld/cat/cash/CatAndCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhworld.cat.cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatAndCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
