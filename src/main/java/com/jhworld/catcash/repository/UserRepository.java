7f7610c18ff5bc6b99c3c86a7b5008cd498c8430
SHKim55 <5061ksh@naver.com>
2025-04-09 15:10:43 +0900

    feat : add repository for managing user data

---
+++ src/main/java/com/jhworld/catcash/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jhworld.catcash.repository;
+
+import com.jhworld.catcash.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Override
+    Optional<UserEntity> findById(Long aLong);
+
+    Optional<UserEntity> findByUserSequence(String userSequence);
+}
