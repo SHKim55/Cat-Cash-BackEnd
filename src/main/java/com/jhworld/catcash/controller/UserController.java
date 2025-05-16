3e1b3ac4a2bf480c43d0e34d8b93b75c3def21c0
SHKim55 <5061ksh@naver.com>
2025-05-16 15:50:51 +0900

    refactor : change api response type

--- src/main/java/com/jhworld/catcash/controller/UserController.java
+++ src/main/java/com/jhworld/catcash/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.jhworld.catcash.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jhworld.catcash.dto.ResponseObject;
+import com.jhworld.catcash.dto.user.UserDTO;
+import com.jhworld.catcash.dto.user.UserOnboardDTO;
+import com.jhworld.catcash.service.user.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/onboard")
+    public ResponseEntity<ResponseObject> onboardUser(@RequestHeader String token, @RequestBody UserOnboardDTO userOnboardDTO) {
+        return userService.onboardUser(token, userOnboardDTO);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<UserDTO> getUserInfo(@RequestHeader String token) {
+        return userService.getUserInfo(token);
+    }
+
 }
