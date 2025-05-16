bb699b890c2de493b5aa375be27cf10575a9a532
SHKim55 <5061ksh@naver.com>
2025-05-16 15:50:33 +0900

    feat : create table for mapping categories and users

---
+++ src/main/java/com/jhworld/catcash/entity/UserCategoryEntity.java
@@ -0,0 +1,25 @@
+package com.jhworld.catcash.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_category")
+public class UserCategoryEntity {
+    @Id
+    @Column(name = "user_category_id")
+    private Long userCategoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private CategoryEntity category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+}
