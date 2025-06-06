d2125c982d5d789d879987c65500fea13c826963
SHKim55 <5061ksh@naver.com>
2025-06-07 00:18:21 +0900

    hotfix : FCM 설정파일 로딩 불가 문제 해결

--- src/main/java/com/jhworld/catcash/configuration/FirebaseConfig.java
+++ src/main/java/com/jhworld/catcash/configuration/FirebaseConfig.java
@@ -4,18 +4,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Component
-public class FirebaseConfiguration {
+public class FirebaseConfig {
+
+    @Value("${fcm.file_path}")
+    private String firebaseKeyPath;
 
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/cat-cash-firebase-sdk.json");
+            FileInputStream serviceAccount = new FileInputStream(firebaseKeyPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
